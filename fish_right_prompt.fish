e6e68d5491af4ab5908934835c5e0df780385ad4
zhiayang <zhiayang@gmail.com>
2016-06-12 22:48:44 +0800

    modifications.

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,29 +1,32 @@
 
 function get_git_status -d "Gets the current git status"
-  if command git rev-parse --is-inside-work-tree >/dev/null 2>&1
-    set -l dirty (command git status -s --ignore-submodules=dirty | wc -l | sed -e 's/^ *//' -e 's/ *$//' 2> /dev/null)
-    set -l ref (command git describe --tags --exact-match ^/dev/null ; or command git symbolic-ref --short HEAD 2> /dev/null ; or command git rev-parse --short HEAD 2> /dev/null)
+	if command git rev-parse --is-inside-work-tree >/dev/null 2>&1
+		set -l dirty (command git status -s --ignore-submodules=dirty | wc -l | sed -e 's/^ *//' -e 's/ *$//' 2> /dev/null)
+		set -l ref (command git describe --tags --exact-match ^/dev/null ; or command git symbolic-ref --short HEAD 2> /dev/null ; or command git rev-parse --short HEAD 2> /dev/null)
 
-    if [ "$dirty" != "0" ]
-      set_color -b normal
-      set_color red
-      echo "$dirty changed file"
-      if [ "$dirty" != "1" ]
-        echo "s"
-      end
-      echo " "
-      set_color -b red
-      set_color white
-    else
-      set_color -b cyan
-      set_color white
-    end
+		if [ "$dirty" != "0" ]
+			set_color -b normal
+			set_color red
+			echo "changed: $dirty"
+			# if [ "$dirty" != "1" ]
+			# 	echo "s"
+			# end
 
-    echo " $ref "
-    set_color normal
-   end
+			set_color -o white
+			echo " ~ "
+
+			# set_color -b red
+			set_color -o magenta
+		else
+			# set_color -b cyan
+			set_color -o cyan
+		end
+
+		echo "($ref)"
+		set_color normal
+	 end
 end
 
 function fish_right_prompt -d "Prints right prompt"
-  get_git_status
+	get_git_status
 end
